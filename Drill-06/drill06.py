f859c7be129056ba5d554a80504f8aa4c1a7117a
jaewon-lee-ssb <jaewon990905@gmail.com>
2019-10-09 19:07:49 +0900

    Update drill06.py
    2018182029
    commit : 2

--- Drill-06/drill06.py
+++ Drill-06/drill06.py
@@ -1,57 +1,64 @@
 from pico2d import *
 import random
 
-KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+KPU_WIDTH, KPU_HEIGHT = 800, 600
 
-open_canvas(KPU_WIDTH, KPU_HEIGHT)
+open_canvas(1280, 1000)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
-
-
-def draw_curve_4_points(p1, p2, p3, p4):
-    # draw p1-p2
-    global x, y
-    for i in range(0, 50, 2):
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
-
-    # draw p2-p3
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
-
-    # draw p3-p4
-    for i in range(50, 100, 2):
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p2[0] + (-4 * t ** 2 + 4 * t) * p3[0] + (2 * t ** 2 - t) * p4[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p2[1] + (-4 * t ** 2 + 4 * t) * p3[1] + (2 * t ** 2 - t) * p4[1]
-
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
-
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
+cnt = 0
+t = 0
 
 while running:
     clear_canvas()
+    #point = [(random.randint(-500, 500)), (random.randint(-500, 500)),
+    #         (random.randint(-500, 500)), (random.randint(-500, 500)),
+    #         (random.randint(-500, 500)), (random.randint(-500, 500)),
+    #         (random.randint(-500, 500)), (random.randint(-500, 500))]
+    point = (-300, 200, 400, 350, 300, -300, -200, -200)
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    #draw_curve_4_points((random.randint(-1000, 1000)), (random.randint(-1000, 1000)),
-    #(random.randint(-1000, 1000)), (random.randint(-1000, 1000)))
-    draw_curve_4_points((50, 100), (100, 500), (200, -500), (1000, 200))
+    if cnt == 0:
+        if t < 0.5:
+            x = (2 * t ** 2 - 3 * t + 1) * point[0] + (-4 * t ** 2 + 4 * t) * point[2] + (2 * t ** 2 - t) * point[4]
+            y = (2 * t ** 2 - 3 * t + 1) * point[1] + (-4 * t ** 2 + 4 * t) * point[3] + (2 * t ** 2 - t) * point[5]
+            if t == 4.8:
+                t = 0
+                cnt = 1
+    elif cnt == 1:
+        if t < 1:
+            x = ((-t ** 3 + 2 * t ** 2 - t) * point[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[2] + (
+                        -3 * t ** 3 + 4 * t ** 2 + t) * point[4] + (t ** 3 - t ** 2) * point[6]) / 2
+            y = ((-t ** 3 + 2 * t ** 2 - t) * point[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[3] + (
+                        -3 * t ** 3 + 4 * t ** 2 + t) * point[5] + (t ** 3 - t ** 2) * point[7]) / 2
+            if t == 0.98:
+                t = 0.5
+                cnt == 2
+
+    if cnt == 2:
+        if t < 1:
+            x = (2 * t ** 2 - 3 * t + 1) * point[2] + (-4 * t ** 2 + 4 * t) * point[4] + (2 * t ** 2 - t) * point[6]
+            y = (2 * t ** 2 - 3 * t + 1) * point[3] + (-4 * t ** 2 + 4 * t) * point[5] + (2 * t ** 2 - t) * point[7]
+            if t == 0.48:
+                t = 0
+                cnt = 3
+    if cnt == 3:
+        if t < 1:
+            x = ((-t ** 3 + 2 * t ** 2 - t) * point[4] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[6] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * point[0] + (t ** 3 - t ** 2) * point[2]) / 2
+            y = ((-t ** 3 + 2 * t ** 2 - t) * point[5] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[7] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * point[1] + (t ** 3 - t ** 2) * point[3]) / 2
+            if t == 0.98:
+                t = 0
+                cnt = 4
     character.clip_draw(frame * 100, 100, 100, 100, x, y)
     update_canvas()
     delay(0.05)
     frame = (frame + 1) % 8
+    t += 0.02
 
 close_canvas()
