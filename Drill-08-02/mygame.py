506c5b0f7c4806218f494e679d2d74ec7d56d5ea
jaewon-lee-ssb <jaewon990905@gmail.com>
2019-11-12 18:30:41 +0900

    2018182029 이재원 Drill 10 완성
    https://youtu.be/oXvTGMJ1s-Y
    commit : 1

--- Drill-08-02/mygame.py
+++ Drill-08-02/mygame.py
@@ -4,8 +4,8 @@ import game_framework
 
 import pico2d
 
-import start_state
+import title_state
 
 pico2d.open_canvas()
-game_framework.run(start_state)
+game_framework.run(title_state)
 pico2d.close_canvas()
